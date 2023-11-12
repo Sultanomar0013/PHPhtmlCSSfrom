47de22de797bb34e071511481c4c27d861b3f047
Sultanomar0013 <sultanomar756@gmail.com>
2023-11-12 21:57:35 +0600

    form

---
+++ form.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+    <form>
+    <div class="text-center">
+    <p style="display: inline-block;">Date</p> <p class="text-right" style="display: inline-block; margin-left: 550px; margin-top: 50px;">Ref No: RPAEL/HR/MR/23/</p>
+    </div>
+    <div style= "margin-left: 500px;"><h4>MEMBER HIRING REQUISITION FORM</h4></div>
+
+    <div style="display: inline-block; margin-left: 300px;"> <label > 1. Unit/Department </label>  
+    <select class="" id="exampleSelect"  style=" margin-left: 50px; width:500px; height:30px;">
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+    </div>
+    <div style="display: inline-block; margin-left: 300px;margin-top: 15px;"> <label >*Location </label>  
+    <select class="" id="exampleSelect"  style=" margin-left: 110px; width:500px; height:30px;">
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+    </div>
+    <div style="display: inline-block; margin-left: 300px;margin-top: 15px;"> <label > 1. Unit/Department </label>  
+    <select class="form-control selectpicker" data-live-search="true" id="exampleSelect"  style=" margin-left: 50px; width:500px; height:30px;">
+      <option>kol1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+    </div>
+    <div style="display: inline-block; margin-left: 300px;margin-top: 15px;"> <label > 1. Unit/Department </label>  
+    <select class="" id="exampleSelect"  style=" margin-left: 50px; width:500px; height:30px;">
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+    </div>
+
+   </form>
+   
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js" integrity="sha512-CwHUCK55pONjDxvPZQeuwKpxos8mPyEv9gGuWC8Vr0357J2uXg1PycGDPND9EgdokSFTG6kgSApoDj9OM22ksw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  
+</body>
+</html>
